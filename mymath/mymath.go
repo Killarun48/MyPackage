eee3d4928e60d59f9a9c7fa19fcb328e5036f9c8
Kil <kil@kil.com>
2024-07-21 20:49:56 +0300

    v2.0

--- mymath/mymath.go
+++ mymath/mymath.go
@@ -2,6 +2,14 @@ package mymath
 
 import "math"
 
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
+
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
